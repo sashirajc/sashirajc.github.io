18bcfbbf773177c7c0579eb4bae52bb2e50b9321
Sashiraj Chandrasekaran <sashiraj@nalagenetics.com>
2022-07-30 09:26:42 +0800

    Updated GTM plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,37 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingId: process.env.GTA,
+          // process.env.GAS, // Google Analytics / GA / GAS
+          
+          // process.env.GAD, // Google Ads / Adwords / AW / GAD
+          // process.env.MAF, // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager) / MAF
+        // ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: process.env.GTA,
+          anonymize_ip: true,
+          cookie_expires: 0,
+          send_page_view: true
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: false,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://sashirajc.github.io",
+        },
+      },
+    },
+    {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
@@ -146,35 +177,6 @@ module.exports = {
             title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
           },
         ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          process.env.GAS, // Google Analytics / GA / GAS
-          process.env.GAD, // Google Ads / Adwords / AW / GAD
-          process.env.MAF, // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager) / MAF
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://sashirajc.github.io",
-        },
       },
     },
     {
